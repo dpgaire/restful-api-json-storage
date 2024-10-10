e9bb814061706025071f686d8e46fcb6abc0390c
dpgaire <gairhedurga13@gmail.com>
2024-10-10 20:54:35 +0545

    add vercel config file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js
 const express = require("express");
-const snippetsRoutes = require("./routes/snippets");
+const snippetsRoutes = require("./api/snippets");
 const app = express();
 
 // Middleware to parse incoming JSON requests
@@ -8,6 +8,9 @@ app.use(express.json());
 
 // Mount the snippets routes
 app.use("/api/snippets", snippetsRoutes);
+app.use("/", (req, res) => {
+  res.send("API is running......");
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
